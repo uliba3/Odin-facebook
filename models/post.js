0103be98cdc42fc86bbdc6e48d91a115a1ff5d91
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-08-29 10:32:10 +0900

    cut relation betweeen user and post schema

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = ({
-    author: { type: Schema.Types.ObjectId, ref: "Author", required: true},
+    author: { type: String, required: true},
     content: { type: String, required: true},
     postedDate: { type: Date },
     root: { type: Schema.Types.ObjectId, ref: "Post"},
---
+++ routes/catalog.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+/// OdinBookFacebook ///
+//router.get("/", );

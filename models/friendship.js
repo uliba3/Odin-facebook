a9e864fcab78535848a5ea00a904c3aaafd727b9
uliba3 <60570852+ulilba3@users.noreply.github.com>
2023-09-01 11:04:31 +0900

    create frinedship schema

---
+++ models/friendship.js
@@ -0,0 +1,13 @@
+//models/friendship.js
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const FriendshipSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the user who initiated the friend request
+    friend: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the friend being added
+    status: { type: String, enum: ['pending', 'accepted'], default: 'pending' }, // Friendship status
+  });
+
+// Export model
+module.exports = mongoose.model("Friendship", FriendshipSchema);
